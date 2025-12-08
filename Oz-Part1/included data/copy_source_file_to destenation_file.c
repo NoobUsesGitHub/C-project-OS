0211ed6201e07d7f415c9ae9d4497846d9c32b77
Noob <ozbm1000@gmail.com>
2025-12-08 21:42:42 +0200

    including data

---
+++ Oz-Part1/included data/copy_source_file_to destenation_file.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <sys/fcntl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#define SIZE 10 
+//Copy file example
+//You nee to create a source.txt file with content to be copy to destenation.txt file
+int main(int argc , char* argv[]) 
+{
+	int fdin;           /* input file descriptor */ 
+	int fdout;          /* out   file descriptor */ 
+	char buf[SIZE+1]; /* input (output) buffer */
+	int charsr;          /* how many chars were actually read */ 
+	int charsw;          /* how many chars were actually written */ 
+
+
+	fdin = open("source.txt",O_RDONLY);
+	if (fdin < 0) /* means file open did not take place */ 
+	{                
+		perror("after open ");   /* text explaining why */ 
+		exit(-1); 
+	}
+	/* create the file with read write premissions */
+	fdout = open("destenation.txt", O_CREAT | O_RDWR, 0666);  	
+	if (fdout < 0)	/* means file open did not take place */ 
+	{ 
+		perror("after create ");   /* text explaining why */ 
+		exit(-1); 
+	}
+
+	do 
+	{
+		charsr = read(fdin,buf,SIZE);
+		/* why writting SIZE can be wrong...*/
+		charsw = write(fdout,buf,charsr);		
+		if (charsw < charsr) 
+		{
+			printf("error writing\n");
+		} 
+	} while ( (charsr == SIZE) && (charsw == SIZE));
+	close(fdin);		/* free allocated structures */
+	close(fdout);		/* free allocated structures */
+}
+
