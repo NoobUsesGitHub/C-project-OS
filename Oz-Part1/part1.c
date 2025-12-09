52c05043b884d4d4f4875026416cf473e2573894
Oz Ben Moshe <Ozbm1000@gmail.com>
2025-12-09 10:13:54 +0000

    done

--- Oz-Part1/part1.c
+++ Oz-Part1/part1.c
@@ -2,7 +2,6 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 #define READSIZE 1
 
@@ -25,19 +24,18 @@ int main(int argc,char *argv[] )
     int foundDiff=0;
     int readOutput1;
     int readOutput2;
-    int x;
     
     memset(buffer2,0,READSIZE+1);//resetting the buffer
     memset(buffer1,0,READSIZE+1);
     if(argc!=3 ){
         //implement error catch of less or more than 2 files input
-        perror("not correct amount of arguments");
+        //perror("not correct amount of arguments");
         return 1;
     }
     int fd1=open(argv[1],O_RDONLY);
     int fd2=open(argv[2],O_RDONLY);
     if(fd1 == -1 ||fd2==-1 ){
-        perror("open failed");
+        //perror("open failed");
         return 1;
     }
 
@@ -48,18 +46,17 @@ int main(int argc,char *argv[] )
     }while(readOutput1>0&&readOutput2>0&&foundDiff==0);//read(fd1,buffer1,READSIZE)>0 &&read(fd2,buffer2,READSIZE)>0
 
     if(readOutput1==-1 || readOutput2==-1){
-        perror("failed reading");
+        //perror("failed reading");
         closeFiles(fd1,fd2);
         return 1;
     }
 
     if((readOutput1==0 && readOutput2>0)||(readOutput1>0 && readOutput2==0)){
         //not the same size, reached EOF before the other one
-        perror("files not the same size");
+        //perror("files not the same size");
         closeFiles(fd1,fd2);
         return 1;
     }
-    
     closeFiles(fd1,fd2);
     return foundDiff==1?1:2;
 }
