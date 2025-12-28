e1b5a9c292e20b8f6a00a5759ae67b0cc73ed97a
Oz Ben Moshe <Ozbm1000@gmail.com>
2025-12-28 06:55:32 +0000

    added test data

---
+++ Oz-Part1/input data/elmalea/3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+printf("Please enter operation\n
+Pleasaaaaaaaaaaaaaaaaaaaaaaaao numbers\n
+The sum is 7\n
+Please enter operation\n
+Bye");}
