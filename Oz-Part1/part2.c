637da9f2917562b184d59619c61321552529e0df
Oz Ben Moshe <Ozbm1000@gmail.com>
2025-12-29 21:40:59 +0000

    almost there

--- Oz-Part1/part2.c
+++ Oz-Part1/part2.c
@@ -36,9 +36,10 @@ int compiler(char *program){
             else
                 if(pid==0){
                     //child - compiler
-                    strcat(program,"*.c");
-                    char* args[]={"gcc",program,"-o","main.exe",NULL};
-                    execvp("gcc",args);        
+                    char cmd[4096];
+                    snprintf(cmd, sizeof(cmd), "gcc '%s'/*.c -o main.exe", program);
+                    char *args[] = {"sh", "-c", cmd, NULL};
+                    execvp("sh", args);    
                 }
             pid_t waitTmp=waitpid(pid,&status,0);//checks if child finished successfully
             if(waitTmp==-1)
@@ -47,7 +48,6 @@ int compiler(char *program){
                     returnCode=WEXITSTATUS(status);
                 else
                     returnCode=0;
-            printf("%d",returnCode);
             if(returnCode!=0)
                 return 0;
 
@@ -68,7 +68,7 @@ int runProcessAndCompareOutput(char *program, char *input, char *output){
         if(pid==0)
         {
             close(1);
-            fd=open("folderNames.text",(O_WRONLY|O_CREAT|O_TRUNC),0666);
+            fd=open("programOutput.text",(O_WRONLY|O_CREAT|O_TRUNC),0666);
             dup(fd);
             close(0);
             fd2=open(input,O_RDONLY,0);
@@ -93,16 +93,19 @@ int runProcessAndCompareOutput(char *program, char *input, char *output){
         {
             //child - compare
             char* args[]={"./comp.out",output,"programOutput.txt",NULL};
+
+        printf("\n!!!!testing file %s!!!!\n",output);//test print
             execvp("./comp.out",args);
         }
     pid_t waitTmp=waitpid(pid,&status,0);//checks if files are equal
     if(waitTmp==-1)
         perror("waitpid failed");
     else if(WIFEXITED(status))
+        {
         returnCode=WEXITSTATUS(status);
-        else
+        printf("\n!!!!testing file %d!!!!\n",returnCode);//test print
+        }else
             returnCode=0;
-    printf("%d",returnCode);//test print
     if(returnCode!=2)
         return 0;
     return 1;
@@ -139,7 +142,6 @@ int main(int argc,char *argv[])
     programFolder[strcspn(programFolder, "\r\n")] = '\0';
     inputFile[strcspn(inputFile, "\r\n")] = '\0';
     outputFile[strcspn(outputFile, "\r\n")] = '\0';
-    
     // printf("%s",programFolder);
     // printf("%s",inputFile);
     // printf("%s",outputFile);
@@ -148,8 +150,8 @@ int main(int argc,char *argv[])
     else
         if(pid==0){
             //child- will run ls with dup to close the stdout
-            close(STDOUT_FILENO);//closes ouput
             int fd=open("folderNames.text",(O_WRONLY|O_CREAT|O_TRUNC),0666);//create new file
+            close(STDOUT_FILENO);//closes ouput
             dup(fd);
             // stringBufferSize=snprintf(NULL,0,"ls %s %s",programFolder,"> folderNames.text");
             // stringBuffer=malloc(sizeof(char)*stringBufferSize+1);
@@ -158,7 +160,6 @@ int main(int argc,char *argv[])
             close(fd);
             execvp("ls",args);
             // free(stringBuffer);
-
             exit(-1);//failed
         }
     waitpid(pid,NULL,0);
@@ -168,7 +169,6 @@ int main(int argc,char *argv[])
     FILE *folderFile=fileOpenerFromPath("","./folderNames.text","r");
     //FILE *gradeOutputFile=fileOpenerFromPath(programFolder,"/results.csv","w");
     FILE *gradeOutputFile=fileOpenerFromPath("","./results.csv","w");
-
     char programName[BUFFERSIZE*2];
     
     while(fgets(currentFile,sizeof(currentFile),folderFile)!=NULL){
@@ -195,7 +195,7 @@ int main(int argc,char *argv[])
         programName[0]='\0';
         stringBufferSize=snprintf(NULL,0,"%s,%d\n",currentFile,0);
         studentOutput=malloc(sizeof(char)*stringBufferSize+1);
-        sprintf(studentOutput,"%s,%d",currentFile,grade); 
+        sprintf(studentOutput,"%s,%d\n",currentFile,grade); 
         if(fwrite(studentOutput,sizeof(char),strlen(studentOutput),gradeOutputFile)<=strlen(studentOutput))
             perror("writing Error");
         free(studentOutput);
