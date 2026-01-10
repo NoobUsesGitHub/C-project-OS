325905308266bb3f57d14078968ac8cd86185e65
David Norman <davnor10@gmail.com>
2026-01-10 19:27:57 +0200

    created a directory for each exercise, with a config file, example_files, part_01 folder and instructions as pdf

---
+++ ex01/example_files/input_data/dolev/2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("Please enter operation\n
+    Please enter two numbers\n
+    The sum is 7\n
+    Please enter operation\n
+    Bye");
+}
